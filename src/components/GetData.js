a42b0f62dbbab67f41bf6599a3295604101c763b
KamilPawel <ks6g18@soton.ac.uk>
2021-08-05 02:01:32 +0100

    visualisation working

--- src/components/GetData.js
+++ src/components/GetData.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import axios from 'axios'
 import Plotter from './Plotter';
+import Test from './Test'
 
 const API_LINK = (
     'https://api.coronavirus.data.gov.uk/v1/data?' +
@@ -27,24 +28,9 @@ export class GetData extends Component {
         this.state = {
             covidData: [],
             dates: [],
-            // For testing
-            rawData: '',
-            link: API_LINK
-       // this.componentDidMount = this.componentDidMount.bind(this)
     }
 }
-/*
-    componentDidMount = () => {
-        axios.get(API_LINK).then(res => {
-            console.log(res.data.data[0])
-            this.setState({
-                covidData: res.data.data,
-                rawData: res
-                })
-            })
-        }
-  
-*/
+
     componentDidMount = () => {
         axios.get(API_LINK).then(res => {
             console.log(res.data.data)
@@ -57,13 +43,45 @@ export class GetData extends Component {
 
 
     
+    //<Plotter inputData = {this.state.covidData}/>
     
     render() {
         window.state = this.state
+
+        const d = {
+                labels: this.state.covidData.map(element => element.date).reverse(),
+                datasets: [
+                    {
+                    type: 'line',
+                    label: 'Number of New Covid Cases',
+                    borderColor: 'rgba(0, 0, 0, 0)',
+                    backgroundColor: 'red',
+                    borderWidth: 2,
+                    fill: false,
+                    data: this.state.covidData.map(element => element.newCases).reverse()
+                    },
+                    {
+                    type: 'line',
+                    label: 'Rate of Covid Cases Increasing',
+                    backgroundColor: 'cyan',
+                    data: this.state.covidData.map(element => element.rateCases).reverse(),
+                    borderColor: 'rgba(0, 0, 0, 0)',
+                    borderWidth: 2,
+                    },
+                    /*{
+                    type: 'line',
+                    label: 'Dataset 3',
+                    backgroundColor: 'rgb(75, 192, 192)',
+                    data: [1,2,2],
+                    },*/
+                ],
+            };
+
+
         return (
             <div>
                 <h4>Plotter</h4>
-                <Plotter inputData = {this.state.covidData}/>
+                <Test x = {d} y = {[1,2,3]}/>
                 <h4>Data Collected from the API</h4>
                 <table>
                     <tr>
@@ -91,5 +109,7 @@ export class GetData extends Component {
         )
     }
 }
+
+
 
 export default GetData
--- src/components/Plotter.js
+++ src/components/Plotter.js
@@ -1,37 +1,68 @@
 import React from 'react'
 import { Line } from 'react-chartjs-2'
-
+import { useState } from 'react';
 
 
 const Plotter = (props) => {
     const data = {
-      labels: props.inputData.map(element => element.date),
+      labels: props.inputData.map(element => element.date).reverse(),
       datasets: [
         {
           label: 'Number of New COVID Cases',
-          data: props.inputData.map(element => element.newCases),
+          data: props.inputData.map(element => element.newCases).reverse(),
           fill: false,
           backgroundColor: 'rgb(255, 99, 132)',
           borderColor: 'rgba(255, 99, 132, 0.2)',
         },
+        {
+            label: "Number of Rate COVID Cases",
+            data: props.inputData.map(element => element.rateCases).reverse(),
+            fill: false,
+            backgroundColor: 'rgb(80, 255, 100)',
+            borderColor: 'rgba(80, 255, 100, 0.2)'
+        }
       ],
     };
-    
+
+
     const options = {
-      scales: {
-        yAxes: [
-          {
-            ticks: {
-              beginAtZero: true,
-            },
-          },
-        ],
-      },
+        scales: {
+            yAxes: [
+                {
+                    ticks: {
+                        beginAtZero: true,
+                    },
+                },
+            ],
+        },
     };
-    
+    const [dataState, setData] = useState(data)
+
+    const handleClick = () => {
+        const newData = data
+        if (data.datasets[0] === "") {
+            console.log("Change this to real value")
+            newData.datasets[0].data = props.inputData.map(element => element.newCases).reverse()
+        }
+        else {
+            console.log("Change this to ''")
+            newData.datasets[0].data = ""
+        }
+        setData(newData)
+        console.log(newData.datasets)
+        console.log('clicked')
+        return (
+            <div className = 'plot'>
+                <Line redraw = {true} data={data} options={options} />
+                <button onClick = {handleClick}>Get Data</button>
+            </div>            
+        )
+    }
+    window.d = dataState
     return (
             <div className = 'plot'>
                 <Line data={data} options={options} />
+                <button onClick = {handleClick}>Get Data</button>
             </div>
     )
 };
